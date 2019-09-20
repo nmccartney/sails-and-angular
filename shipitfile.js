3c55dae2d47366137b0418cdc225f7a1ba6aead7
nmccartney <nmccartney1231@gmail.com>
2019-09-20 15:35:31 -0400

    update key

--- shipitfile.js
+++ shipitfile.js
@@ -12,7 +12,7 @@ module.exports = function (shipit) {
       ignores: ['.git', 'node_modules'],
       rsync: ['--del'],
       keepReleases: 2,
-      key: '../.ssh/id_rsa',
+      key: '~/.ssh/id_rsa',
       shallowClone: true
     },
     staging: {
